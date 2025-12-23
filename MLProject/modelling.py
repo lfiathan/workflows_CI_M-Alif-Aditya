ac3a6800fad42d161ff76d14168e95662000913a
lfiathan <muhammadalifaditya92@gmail.com>
2025-12-23 16:24:41 +0700

    feat: CI implementation

---
+++ MLProject/modelling.py
@@ -0,0 +1,30 @@
+import argparse
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+from pathlib import Path
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+def train(n_estimators, max_depth):
+    DATA_PATH = Path(__file__).resolve().parent / "winequality_preprocessed.csv"
+    df = pd.read_csv(DATA_PATH)
+    X = df.drop('target', axis=1)
+    y = df['target']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    with mlflow.start_run():
+        model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
+        model.fit(X_train, y_train)
+        
+        # Logging
+        mlflow.log_param("n_estimators", n_estimators)
+        mlflow.log_param("max_depth", max_depth)
+        mlflow.sklearn.log_model(model, "model")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n_estimators", type=int, default=100)
+    parser.add_argument("--max_depth", type=int, default=10)
+    args = parser.parse_args()
+    train(args.n_estimators, args.max_depth)
