93821fdce7ec69b9212ede1f506a5c9e68692ff8
lfiathan <muhammadalifaditya92@gmail.com>
2025-12-24 16:48:18 +0700

    Refactor code changing dataset

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,26 +2,31 @@ import argparse
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-from pathlib import Path
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-
+from sklearn.metrics import accuracy_score
 
 def train(n_estimators, max_depth):
-    DATA_PATH = Path(__file__).resolve().parent / "winequality_preprocessed.csv"
-    df = pd.read_csv(DATA_PATH)
-    X = df.drop('target', axis=1)
-    y = df['target']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    # Load dataset hasil preprocessing laptop
+    df = pd.read_csv('laptop_sales_preprocessed.csv')
+    X = df.drop('price_category', axis=1)
+    y = df['price_category']
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     with mlflow.start_run():
-        model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
+        model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
         model.fit(X_train, y_train)
         
-        # Logging
+        y_pred = model.predict(X_test)
+        acc = accuracy_score(y_test, y_pred)
+        
+        # Logging secara manual untuk Advance
         mlflow.log_param("n_estimators", n_estimators)
         mlflow.log_param("max_depth", max_depth)
+        mlflow.log_metric("accuracy", acc)
         mlflow.sklearn.log_model(model, "model")
+        print(f"Model trained with Accuracy: {acc}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -29,4 +34,3 @@ if __name__ == "__main__":
     parser.add_argument("--max_depth", type=int, default=10)
     args = parser.parse_args()
     train(args.n_estimators, args.max_depth)
-    
